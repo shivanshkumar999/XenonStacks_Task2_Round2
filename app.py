1554735022bfc194d5906a553405a49fd5f88f4e
Shivansh Kumar <67266253+shivanshkumar999@users.noreply.github.com>
2024-01-10 20:36:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 
 mydb = mysql.connector.connect(
-    host= '52.41.36.82',
+    host= '',
     user= 'root',
     password= 'jamesbond990',
     database= 'university',
