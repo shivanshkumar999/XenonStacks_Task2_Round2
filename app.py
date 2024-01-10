39d02627af3c2f2b4f727b062fcbfbc5e6672d29
Shivansh Kumar <67266253+shivanshkumar999@users.noreply.github.com>
2024-01-10 22:09:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from email.mime.text import MIMEText
 import mysql.connector
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
-import MySQLdb.cursors
+import MySQLdb.cursors 
 import os
 
 app = Flask(__name__)
