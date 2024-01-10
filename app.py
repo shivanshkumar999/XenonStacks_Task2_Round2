1eb058c61c66be82238533390819b526e21d7c7e
Shivansh Kumar <shivanshkumar752@gmail.com>
2024-01-10 19:00:57 +0530

    done

--- app.py
+++ app.py
@@ -3,13 +3,24 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import mysql.connector
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
-db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'jamesbond990',
-    'database': 'university',
-}
+app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+    host= 'https://xenonstacksround2task2.onrender.com',
+    user= 'root',
+    password= 'jamesbond990',
+    database= 'university',
+)
+
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'jamesbond990'
+# app.config['MYSQL_DB'] = 'university'
 
 #  Send mail function
 def SendEmail(toaddr,subject,message):
@@ -34,8 +45,6 @@ def SendEmail(toaddr,subject,message):
         s.quit()
 
 
-app = Flask(__name__)
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -52,30 +61,60 @@ def about():
 def contact():
     return render_template('contact.html')
 
+@app.route("/signup",methods=['POST','GET'])
+def signup():
+    return render_template('signup.html')
+
 @app.route("/contactmail", methods=["POST","GET"])
 def contactmail():
     if request.method=='POST':
         name = request.form.get('name')
         email = request.form.get('email')
-        mobile = request.form.get('mobile')
         message = request.form.get('message')
 
     body = f"""\
             <html>
             <body>
             <p>Hello, {name}.<br>
-            Thanks for contacting Shivansh. He will get in touch with you shortly on the following contact details provided by you.</p><br>
+            Thanks for contacting The Gurukul. He will get in touch with you shortly on the following contact details provided by you.</p><br>
             <p>Name : <span style='font-weight:bolder'>{name}</span></p>
             <p>Email : <span style='font-weight:bolder'>{email}</span></p>
-            <p>Mobile No. : <span style='font-weight:bolder'>{mobile}</span></p>
             <p>Message : <span style='font-weight:bolder'>{message}</span></p>
             </body>
             </html>
             """
-    
-    res = f'Thanks for contacting me, {name}'
     SendEmail('shivanshkumar752@gmail.com', f"{name} contacted you via portfolio website", body)
-    return render_template('contact.html', res = res)
+    return render_template('contact.html', res = f'Thanks for contacting Us, {name}. We will get back to you shortly.')
+
+
+@app.route("/signuptest", methods=["POST","GET"])
+def signuptest():
+    if request.method=='POST':
+        uid = request.form.get('name')
+        password = request.form.get('pass')
+
+        cursor = mydb.cursor()
+
+        # Check if the username and password match a record in the database
+        # query = "insert into students (uid, password) values (%s,%s)"
+        query = "insert into students (uid,password) values ( %s, %s)"
+        cursor.execute(query,(uid,password))
+        mydb.commit()
+
+        query = "select * from students where uid = %s and password = %s"
+        cursor.execute(query,(uid,password))
+        student = cursor.fetchone()
+
+        # Close the cursor
+        cursor.close()
+
+    if student:
+        # Successful login
+        res = f"Welcome, {student[0]}!, You are registered Now."
+    else:
+        # Failed login
+        res = "Some error occoured. Please try again."
+    return render_template('loginsuccess.html', res = res)
 
 
 @app.route("/logintest", methods=["POST","GET"])
@@ -84,20 +123,20 @@ def logintest():
         uid = request.form.get('name')
         password = request.form.get('pass')
 
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor()
+        cursor = mydb.cursor()
 
-        query = "SELECT * FROM students"
-        cursor.execute(query, (uid, password))
-        student = cursor.rowcount
+        # Check if the username and password match a record in the database
+        # query = "insert into students (uid, password) values (%s,%s)"
+        query = "select * from students where uid = %s and password = %s"
+        cursor.execute(query,(uid,password))
+        student = cursor.fetchone()
 
-    # Close the database connection
+        # Close the cursor
         cursor.close()
-        conn.close()
 
     if student:
         # Successful login
-        res = f"Welcome, {student[1]}!"
+        res = f"Welcome, {student[0]}!"
     else:
         # Failed login
         res = "Invalid uid or password. Please try again."
@@ -106,7 +145,6 @@ def logintest():
 @app.route("/login")
 def login():
     return render_template('login.html')
-
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0',port=5000)
